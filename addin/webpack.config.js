2d36b6800d9ffe76ab0abd1a839b1cfa35083474
Omkar Waingankar <omkar.waingankar@berkeley.edu>
2025-10-05 21:26:32 -0400

    improved api key setup

--- addin/webpack.config.js
+++ addin/webpack.config.js
@@ -28,6 +28,13 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][ext]'
+        }
       }
     ]
   },
